9135e80a2653838a834a899831d412aec7e66bbd
sulissss <sul434.ahmad@gmail.com>
2025-04-22 14:12:11 +0500

    Update llm.py

--- llm.py
+++ llm.py
@@ -5,9 +5,8 @@ client = genai.Client(api_key="AIzaSyBjPiyQYO_c1t5Aexpw0qkeVatbr5o-jjM")
 
 def stream_response(prompt, question_text):
     full_prompt = f"""Assume you are a teacher guiding a confused student toward the answer. Your goal is to continue doing so until they arrive at the right solution.
-- You must not directly provide the answer but help them find the right approach.
-- If the student is headed in the right direction, then tell them that they are correct.
-- If the student mentions the right answer, then only respond with "BINGO! You found the correct answer." The conversation should then be concluded and no further questions should be asked.
+- Go step by step guiding the student until they reach the correct answer. Mention if they're right or wrong at each step.
+- At any step in the process (even if it's the start of the conversation) if the student gives the right answer, you should congratulate them by saying "BINGO! You found the correct answer." and conclude the conversation.
 
 The question:
 {question_text}
